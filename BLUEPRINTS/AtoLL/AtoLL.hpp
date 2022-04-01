f007ca2998a075492db700e2a3048d338a68a76a
WrathOfXLord <61274689+WrathOfXLord@users.noreply.github.com>
2022-04-01 13:20:13 +0300

    Update AtoLL.hpp

--- BLUEPRINTS/AtoLL/AtoLL.hpp
+++ BLUEPRINTS/AtoLL/AtoLL.hpp
@@ -20,15 +20,15 @@ namespace My {
         uint64_t index {};
         short sign {1};
         int64_t value {0};
-        bool signMark {};
+        bool signUsed {};
 
         if(str[0] == '-') {
             sign = -1;
             ++index;
-            signMark = true;
+            signUsed = true;
         } else if(str[0] == '+') {
             ++index;
-            signMark = true;
+            signUsed = true;
         }
 
         for(; str[index] >= '0' && str[index] <= '9'; ++index) {
@@ -36,9 +36,9 @@ namespace My {
         }
 
         if(value == 0) {
-            if(!signMark && index == 0)
+            if(!signUsed && index == 0)
                 throw InvalidInputException {};
-            else if (signMark && index == 1) {
+            else if (signUsed && index == 1) {
                 throw InvalidInputException {};
             }
         }
